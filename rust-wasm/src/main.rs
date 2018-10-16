12f0249c39af70e3886076c6eafe814a553fae6b
millsky <kmills@vibrenthealth.com>
2018-10-16 09:17:01 -0400

    Feat: (Cocurrency)
    1.) Add a cocurrency example with comments

--- rust-wasm/src/main.rs
+++ rust-wasm/src/main.rs
@@ -1,3 +1,16 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    println!("HELLO FROM COCURRENCY");
+    // Spawn threads
+    let handle = thread::spawn(|| {
+        for i in 1..20 {
+            // Each thread perform an op
+            println!("Hi I spawned from the thread {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    // Wait for all the threads to be done processing and end
+    handle.join().unwrap();
 }
